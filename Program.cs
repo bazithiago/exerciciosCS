0dd1ee30078cc5b4f49cf5f49353819c721c9610
bazithiago <thiagovieira.dev@gmail.com>
2021-10-16 10:58:11 -0300

    Create initial menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,72 @@ namespace exerciciosCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int option;
+            bool validate = true;
+
+            Program program = new Program();
+
+            Console.WriteLine("Exercícios de C#");
+
+            do
+            {
+                Console.WriteLine("\nQual exercício deseja consultar?");
+                Console.WriteLine("(1) IMC");
+                Console.WriteLine("(2) Salário");
+                Console.WriteLine("(3) Volume da esfera");
+                Console.WriteLine("(4) Médias escolares");
+                Console.WriteLine("(5) Cadastro de pets");
+                Console.WriteLine("(6) Sair");
+
+
+                Console.WriteLine("\nDigite sua opção: ");
+                option = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (option)
+                {
+                    case 1:
+                       
+
+                        break;
+
+                    case 2:
+                        
+
+                        break;
+
+                    case 3:
+                        
+
+                        break;
+
+                    case 4:
+                        
+
+                        break;
+
+                    case 5:
+                        
+                        break;
+
+                    case 6:
+                        validate = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+
+                        break;
+                }
+
+
+                Console.WriteLine("\nAperte ENTER para continuar");
+                Console.ReadLine();
+                Console.Clear();
+
+
+            } while (validate);       
+
         }
     }
 }
