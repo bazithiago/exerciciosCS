756f5d34680add821330a3f567bf945d731ccee4
bazithiago <thiagovieira.dev@gmail.com>
2021-10-16 12:22:12 -0300

    Exercise 3

--- Program.cs
+++ Program.cs
@@ -61,7 +61,7 @@ namespace exerciciosCS
                         double minSalary = 1192.4;
                         double result;
 
-                        Console.WriteLine("Qual o seu salário mensal? Digite apenas números, exemplo: 3500.25");
+                        Console.WriteLine("Qual o seu salário mensal? Digite apenas números, ex: 3500.25");
                         salary = Convert.ToDouble(Console.ReadLine());
 
                         result = Math.Round(salary / minSalary, 2);
@@ -70,8 +70,15 @@ namespace exerciciosCS
                         break;
 
                     case 3:
-                        
+                        double radius;
+                        double volume; 
 
+                        Console.WriteLine("Vamos calcular o volume de uma esfera.\nQual o raio (em metros) da sua esfera? Digite apenas números, ex: 0.75");
+                        radius = Convert.ToDouble(Console.ReadLine());
+
+                        volume = Math.Round((4 * 3.1416 * (radius * radius * radius)/3), 2);
+                        Console.WriteLine($"O volume da sua esfera é de {volume} metros cúbicos");
+    
                         break;
 
                     case 4:
